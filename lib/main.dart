b845820476f559755eed747ec9b5e1589f35588b
NormanOsborm <159689265+NormanOsborm@users.noreply.github.com>
2025-08-17 21:37:40 +0530

    conflicts remover

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
-// Usage in your main app:
 import 'package:flutter/material.dart';
-import 'package:paara_app_duk/screens/Kform.dart';
-import 'package:paara_app_duk/screens/homeScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:paara_app_duk/screens/login.dart';
 import 'package:paara_app_duk/screens/splashScreen.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: ParaHomeScreen(),
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData.dark(),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Paara App',
+      home: ParaSplashScreen(),
+    );
+  }
 }
